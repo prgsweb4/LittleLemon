a2bc2d0604e0607f993351f94cc2fc8de23c360d
Abu Rashad <j.jar49n@gmail.com>
2024-02-08 12:12:39 +0300

    Setting routes to users and booking table.

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,32 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.core import serializers
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from datetime import datetime
+
+from .serializers import BookingSerializer, MenuSerializer,UserSerializer
+from .models import Booking, Menu
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User, Group
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Booking.objects.all()
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [IsAuthenticated]
